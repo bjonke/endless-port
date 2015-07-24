f74f1598bc8b312d15b9e1679f8c3fc75701c470
Rickard Skeppstrom <bjonke@users.noreply.github.com>
2015-07-24 08:20:17 +0200

    Update gs_Callback.h

--- gs_Callback.h
+++ gs_Callback.h
@@ -8,16 +8,18 @@ using namespace std;
 class gs_Callback
 {
 private :
-vector< voidfunction> list;
+        vector< voidfunction > list;
 
 public :
-void registerMethod(voidfunction toadd)
-{
-list.push_back(toadd);
-}
-void callAllMethods()
-{
-for ( unsigned int i = 0; i < list.size(); i++)
-list[i]();
-}
+        void registerMethod(voidfunction toadd)
+        {
+                list.push_back(toadd);
+        }
+        void callAllMethods()
+        {
+                for ( unsigned int i = 0; i < list.size(); i++)
+                {
+                        list[i]();
+                }
+        }
 };
